f2e2ccd77e2620301002c3a4ba4f6f2d005012ad
Shweta Roy <royshweta1712@gmail.com>
2024-02-12 11:22:47 +0530

    get watch history controller added

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -3,8 +3,8 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
-
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
@@ -14,7 +14,7 @@ const generateAccessAndRefreshTokens = async (userId) => {
     user.refreshToken = refreshToken;
     // user.accessToken = accessToken
     await user.save({ validateBeforeSave: false });
-    console.log("access_token:",accessToken);
+    console.log("access_token:", accessToken);
     return { accessToken, refreshToken };
   } catch (err) {
     console.log("errror:", err);
@@ -175,132 +175,278 @@ const logoutUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "User logged Out"));
 });
 
-const refreshAccessToken= asyncHandler(async(req,res)=>{
- const incomingrefreshToken= req.cookies.refreshToken||req.body.refreshToken;
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  const incomingrefreshToken =
+    req.cookies.refreshToken || req.body.refreshToken;
 
- if(!incomingrefreshToken){
-  throw new ApiError(401,"unauthorized request")
- }
- try {
-  const decodedToken = jwt.verify(incomingrefreshToken,process.env.REFRESH_TOKEN_SECRET);
- 
-  const user = await User.findById(decodedToken?._id);
-  if(!user){
-   throw new ApiError(401,"Invalid refresh token")
+  if (!incomingrefreshToken) {
+    throw new ApiError(401, "unauthorized request");
   }
-  if(incomingrefreshToken===user?.refreshToken){
-   throw new ApiError(401,"Refresh Token is expired or used.");
-  }
- 
-  const options = {
-   httpOnly:true,
-   secure:true
-  }
- 
-  const {accessToken,newrefreshToken}=await generateAccessAndRefreshTokens(user._id)
-  return res.status(200)
-  .cookie("accessToken",accessToken,options)
-  .cookie("refreshToken",newrefreshToken,options)
-  .json(
-   new ApiResponse(200,
-     {accessToken,newrefreshToken},
-     "Access token refreshed successfully")
-  )
- } catch (error) {
-  console.log("error in try:",error.message);
-  throw new ApiError(401,"Invalid refresh Token");
- }
-})
+  try {
+    const decodedToken = jwt.verify(
+      incomingrefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
 
+    const user = await User.findById(decodedToken?._id);
+    if (!user) {
+      throw new ApiError(401, "Invalid refresh token");
+    }
+    if (incomingrefreshToken === user?.refreshToken) {
+      throw new ApiError(401, "Refresh Token is expired or used.");
+    }
 
-const changeCurrentPassword = asyncHandler(async(req,res)=>{
-  const {oldPassword,newPassword} = req.body;
-  const user = await User.findById(req.user?._id)
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+
+    const { accessToken, newrefreshToken } =
+      await generateAccessAndRefreshTokens(user._id);
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", newrefreshToken, options)
+      .json(
+        new ApiResponse(
+          200,
+          { accessToken, newrefreshToken },
+          "Access token refreshed successfully"
+        )
+      );
+  } catch (error) {
+    console.log("error in try:", error.message);
+    throw new ApiError(401, "Invalid refresh Token");
+  }
+});
+
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+  const user = await User.findById(req.user?._id);
   const PasswordCorrect = await user.isPasswordCorrect(oldPassword);
-  if(!PasswordCorrect){
-    throw new ApiError(400,"Invalid password entered")
+  if (!PasswordCorrect) {
+    throw new ApiError(400, "Invalid password entered");
   }
 
   user.password = newPassword;
-  const a = await user.save({validateBeforeSave:false});
+  const a = await user.save({ validateBeforeSave: false });
   return res
-  .status(200)
-  .json(new ApiResponse(200,"Password has been changed successfully."))
+    .status(200)
+    .json(new ApiResponse(200, "Password has been changed successfully."));
+});
 
-
-})
-
-const getCurrentUser = asyncHandler(async(req,res)=>{
+const getCurrentUser = asyncHandler(async (req, res) => {
   return res
-  .status(200)
-  .json(200,req.user,"current user fetched successfully")
-})
+    .status(200)
+    .json(200, req.user, "current user fetched successfully");
+});
 
-const UpdateAccountDetails = asyncHandler(async(req,res)=>{
-  const {fullName,email}= req.body;
-  if(!fullName||!email){
-    throw new ApiError(400,"All Field are required")
+const UpdateAccountDetails = asyncHandler(async (req, res) => {
+  const { fullName, email } = req.body;
+  if (!fullName || !email) {
+    throw new ApiError(400, "All Field are required");
   }
 
-  const user = await User.findByIdAndUpdate(req.user?._id,
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
     {
-      $set:{
-        fullName:fullName,
-        email:email
-      }
-    },{
-      new:true
+      $set: {
+        fullName: fullName,
+        email: email,
+      },
+    },
+    {
+      new: true,
     }
-    ).select("-password");
+  ).select("-password");
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated successfully"));
+});
+
+const updateUserAvatar = asyncHandler(async (req, res) => {
+  // get the new avtar file from the frontend using multer
+  // save it on cloud
+  // find user
+  // replace cloud url with new one
+  const newAvatarFile = req.file?.path;
+  if (!newAvatarFile) {
+    throw new ApiError(401, "Avatar file is required for path");
+  }
+  const newCloudinary = await uploadOnCloudinary(newAvatarFile);
+  if (!newCloudinary.url) {
+    throw new ApiError(402, "Error while uploading on avatar");
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        avatar: newCloudinary.url,
+      },
+    },
+    { new: true }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "avtar file changed successfully."));
+});
+
+const updateUserCoverImage = asyncHandler(async (req, res) => {
+  // get the new avtar file from the frontend using multer
+  // save it on cloud
+  // find user
+  // replace cloud url with new one
+  const newcoverImage = req.file?.path;
+  if (!newcoverImage) {
+    throw new ApiError(401, "cover Image is required for path");
+  }
+  const newCloudinary = await uploadOnCloudinary(newcoverImage);
+  if (!newCloudinary.url) {
+    throw new ApiError(402, user, "Error while uploading on avatar");
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        coverImage: newCloudinary.url,
+      },
+    },
+    { new: true }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "cover Image file changed successfully."));
+});
+
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+  if (username?.trim()) {
+    throw new ApiError(401, "UserProfile url not provided.");
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase(),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields:{
+        subscribersCount:{$size:"$subscribers"},
+        channelsSubscribedTo:{
+          $size:"$subscribedTo"
+        },
+        isSubscribed:{
+          $cond:{
+            if:{$in:[req.user?._id,"$susbscribers.subscriber"]},
+            then:true,
+            else:false
+          }
+        }
+      },
+
+    },
+    {
+      $project:{
+        fullName:1,
+        username:1,
+        subscribersCount:1,
+        isSubscribed:1,
+        avatar:1,
+        coverImage:1,
+        email:1,
+        channelsSubscribedTo:1
+
+      }
+    }
+  ]);
+
+  if(!channel?.length){
+    throw new ApiError(401,"channel doesn't exists.")
+  }
   return res.status(200)
-  .json(new ApiResponse(200,user,"Account details updated successfully"))
-})
+  .json(new ApiResponse(200,channel[0],"User channnel fetched successfully."))
+});
 
-const updateUserAvatar = asyncHandler(async(req,res)=>{
-  // get the new avtar file from the frontend using multer 
-  // save it on cloud
-  // find user
-  // replace cloud url with new one
- const newAvatarFile =  req.file?.path;
- if(!newAvatarFile){
-  throw new ApiError(401,"Avatar file is required for path")
- }
- const newCloudinary = await uploadOnCloudinary(newAvatarFile);
- if(!newCloudinary.url){
-  throw new ApiError(402,"Error while uploading on avatar")
- }
+const getWatchHistory = asyncHandler(async(req,res)=>{
 
- const user=await User.findByIdAndUpdate(req.user?._id,{
-  $set:{
-    avatar:newCloudinary.url
-  }
- },{new:true}).select("-password");
+ const user =  await User.aggregate([
+  {
+    $match:{
+      _id:new mongoose.Types.ObjectId(req.user._id)
+    }
+  },
+  {
+    $lookup:{
+      from :"videos",
+      localField:"watchHistory",
+      foreignField:"_id",
+      as:"watchHistory",
+      pipeline:[
+        {
+          $lookup:{
+            from:"users",
+            localField:"owner",
+            foreignField:"_id",
+            as:"owner",
+            pipeline:[
+              {
+                $project:{
+                  fullName:1,
+                  username:1,
+                  avatar:1
+                }
+              }
+            ]
+          }
+        },
+        {
+          $addFields:{
+            owner:{
+              $first:"$owner"
+            }
+          }
+        }
+      ]
+    }
+  },
+
+ ])
 
  return res.status(200)
- .json(new ApiResponse(200,"avtar file changed successfully."))
+ .json(new ApiResponse(201,user[0].watchHistory,"User watched history fetched successfully."))
 })
 
-const updateUserCoverImage= asyncHandler(async(req,res)=>{
-  // get the new avtar file from the frontend using multer 
-  // save it on cloud
-  // find user
-  // replace cloud url with new one
- const newcoverImage =  req.file?.path;
- if(!newcoverImage){
-  throw new ApiError(401,"cover Image is required for path")
- }
- const newCloudinary = await uploadOnCloudinary(newcoverImage);
- if(!newCloudinary.url){
-  throw new ApiError(402,user,"Error while uploading on avatar")
- }
-
- const user=await User.findByIdAndUpdate(req.user?._id,{
-  $set:{
-    coverImage:newCloudinary.url
-  }
- },{new:true}).select("-password");
-
- return res.status(200)
- .json(new ApiResponse(200,user,"cover Image file changed successfully."))
-})
-export { registerUser, loginUser, logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,UpdateAccountDetails,updateUserAvatar ,updateUserCoverImage};
+export {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  UpdateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory
+};
